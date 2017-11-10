0ae2d19d41a081e8de8ad855e54dde8275807c1b
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2017-11-10 07:24:37 -0800

    moved gameboard generation to javascript instead of static html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 
 
 
+const makeBoard = () =>
+{
+    const $gameBoard = $('#gameBoard');
+    for (let i = 0; i < 7; i++)
+    {
+        // Generate a new column and append the arrow
+        const $column = $('<div>').addClass('column');
+        $column.append($('<div>').addClass('arrow'));
+
+        // Append 6 slots to each column below the arrow
+        for (let j = 0; j < 6; j++)
+        {
+            $column.append($('<div>').addClass('slot'));
+        }
+        console.log("wubalubadubdub");
+        $gameBoard.append($column);
+    }
+}
+
 
 $(() => {
 
-
+    // Dynamic generate the column divs and populate them with an arrow and slots
+    makeBoard();
 
 
 });
